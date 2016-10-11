57439cfce5087ac9cd866f5086f430883175ea92
chris <chojinchris@gmail.com>
2016-10-11 20:22:33 +0200

    Changement pour la methode Path.GetFileName() qui renvoie directement le bon nom de fichier sans le chemin complet

--- Program.cs
+++ Program.cs
@@ -19,27 +19,29 @@ namespace SuperNesGameList
                 List<string> GoodList = new List<string>();
                 List<string> BadList = new List<string>();
 
-                filenames = Directory.EnumerateFiles(path);                
+                filenames = Directory.EnumerateFiles(path);   
+                             
                 
                 foreach (var item in filenames)
-                {                   
-                     
+                {
+
+                   var fileName =  Path.GetFileName(item);
                    //On va séparer le nom de fichier du nom du répertoire                       
                    var res = Regex.Split(item, @"(.*Set)\\(.*\.smc)");
 
                     if (res.Count() > 1)
                     {
                         //Tri sur le format du string avec les Regex
-                        Match match = Regex.Match(res[2], @"^.{1}[^S]{1}.*(\[!\]\.smc$)|(^.{1}[^S]{1}.*\(J\) \(V1\.0\)\.smc$)|(^.{1}[^S]{1}.*\(J\)\.smc$)|(^.{1}[^S]{1}.*\(U\)\.smc$)|(^.{1}[^S]{1}.*\(E\)\.smc$)|(^.{1}[^S]{1}.*\(F\)\.smc$)|(^.{1}[^S]{1}.*\(F\) \(V1\.0\)\.smc$)");
+                        Match match = Regex.Match(fileName, @"^.{1}[^S]{1}.*(\[!\]\.smc$)|(^.{1}[^S]{1}.*\(J\) \(V1\.0\)\.smc$)|(^.{1}[^S]{1}.*\(J\)\.smc$)|(^.{1}[^S]{1}.*\(U\)\.smc$)|(^.{1}[^S]{1}.*\(E\)\.smc$)|(^.{1}[^S]{1}.*\(F\)\.smc$)|(^.{1}[^S]{1}.*\(F\) \(V1\.0\)\.smc$)");
                         
                         //Si on rentre pas dans les conditions
                         if (!match.Success)
                         {
                             using (StreamWriter strW = new StreamWriter("SuperNesBadGameList.txt",true))
                             {
-                                strW.WriteLine(item);
+                                strW.WriteLine(fileName);
                             }
-                            BadList.Add(item);
+                            BadList.Add(fileName);
                         }
                         //Sinon C'est le format recherché
                         else
